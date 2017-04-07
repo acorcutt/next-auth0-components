16db6e250e6db2ba984cb8c2f6ca61c27e311fe7
Anthony Corcutt <acorcutt@me.com>
2017-04-07 12:54:41 +0000

    cleanup env example

--- env.js
+++ env.js
@@ -2,8 +2,5 @@
 // Restart server after modification
 // via https://github.com/zeit/next.js/tree/master/examples/with-universal-configuration
 module.exports = {
-  "process.env.GRAPHQL_URL": process.env.GRAPHQL_URL,
-  "process.env.AUTH0_DOMAIN": process.env.AUTH0_DOMAIN,
-  "process.env.AUTH0_ID": process.env.AUTH0_ID,
   "process.env.NODE_ENV": process.env.NODE_ENV
 };
