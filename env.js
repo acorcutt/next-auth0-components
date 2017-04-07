a13b01efc9f4e3b049b4260ba83301c365560150
Anthony Corcutt <acorcutt@me.com>
2017-04-07 12:54:02 +0000

    fix for env and npm packager

---
+++ env.js
@@ -0,0 +1,9 @@
+// This sets variables we want available in client and server env, to be transformed by babel replace
+// Restart server after modification
+// via https://github.com/zeit/next.js/tree/master/examples/with-universal-configuration
+module.exports = {
+  "process.env.GRAPHQL_URL": process.env.GRAPHQL_URL,
+  "process.env.AUTH0_DOMAIN": process.env.AUTH0_DOMAIN,
+  "process.env.AUTH0_ID": process.env.AUTH0_ID,
+  "process.env.NODE_ENV": process.env.NODE_ENV
+};
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+// Point at something in .next/dist if you want the transpiled es5 code as a module
 export default null;
