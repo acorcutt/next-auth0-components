6bf129a731d902ebf4fbd965644e1d08424ca57c
Anthony Corcutt <acorcutt@me.com>
2017-04-24 16:21:26 +0000

    add lock to exports

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-// Point at something in build/next/dist if you want an ES5 module
-module.exports = require('./build/next/dist/app');
+// Lock
+module.exports.Lock = require('./.next/dist/components/Lock');
